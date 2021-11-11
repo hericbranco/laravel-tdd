605eb21c9c7606626b30d1b0827e34e6ecacfb3b
Heric Branco <hericbranco@gmail.com>
2021-11-10 22:05:18 -0300

    Final da aula 03

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -12,5 +12,20 @@
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                @foreach($thread->replies as $reply)
+                    <div class="card">
+                        <div class="card-header">
+                            <a href="">{{$reply->owner->name}}</a> said{{$reply->created_at->diffForHumans()}}
+                        </div>
+                        <div class="card-body">
+                            {{$reply->body}}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
