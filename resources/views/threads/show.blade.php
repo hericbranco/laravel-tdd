853ec0037c687c12d9da6d271b99677f74329050
Heric Branco <hericbranco@gmail.com>
2021-11-11 21:19:54 -0300

    FIm da aula 06

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -23,5 +23,22 @@
                 @endforeach
             </div>
         </div>
+
+        @if (auth()->check())
+            <div class="row">
+                <div class="col-md-12">
+                    <form method="POST" action="{{$thread->path().'/replies'}}">
+                        {{csrf_field()}}
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="body" rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{route('login')}}">sign in</a>.</p>
+        @endif
     </div>
 @endsection
