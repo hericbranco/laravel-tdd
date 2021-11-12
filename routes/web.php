c1d1384020d38630d2049d9fa5a8716d53912d7a
Heric Branco <hericbranco@gmail.com>
2021-11-11 21:39:39 -0300

    Fim da aula 06

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/threads', [ThreadsController::class, 'index']);
+Route::post('/threads', [ThreadsController::class, 'store']);
 Route::get('/threads/{thread}', [ThreadsController::class, 'show']);
 Route::post('/threads/{thread}/replies', [\App\Http\Controllers\RepliesController::class, 'store']);
---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Thread;
+use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function guests_may_not_create_threads()
+    {
+        $this->expectException(AuthenticationException::class);
+        $thread = Thread::factory()->make();
+        $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->actingAs(User::factory()->create());
+        $thread = Thread::factory()->make();
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
